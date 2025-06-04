09c3e841fcda3a01c5f0b63d4510fc76f7716375
Guillaume Vincent <23073977+guivin@users.noreply.github.com>
2025-06-04 22:38:53 +0200

    feat: Add `bootstrap_brokers_vpc_connectivity_*` to outputs (#54)
    Co-authored-by: Bryant Biggs <bryantbiggs@gmail.com>

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -52,6 +52,26 @@ output "bootstrap_brokers_public_tls" {
   value       = module.msk_cluster.bootstrap_brokers_public_tls
 }
 
+output "bootstrap_brokers_vpc_connectivity" {
+  description = "Comma separated list of one or more hostname:port pairs of Kafka brokers suitable to bootstrap connectivity to the Kafka cluster"
+  value       = module.msk_cluster.bootstrap_brokers_vpc_connectivity
+}
+
+output "bootstrap_brokers_vpc_connectivity_sasl_iam" {
+  description = "One or more DNS names (or IP addresses) and SASL IAM port pairs for VPC connectivity"
+  value       = module.msk_cluster.bootstrap_brokers_vpc_connectivity_sasl_iam
+}
+
+output "bootstrap_brokers_vpc_connectivity_sasl_scram" {
+  description = "One or more DNS names (or IP addresses) and SASL SCRAM port pairs for VPC connectivity"
+  value       = module.msk_cluster.bootstrap_brokers_vpc_connectivity_sasl_scram
+}
+
+output "bootstrap_brokers_vpc_connectivity_tls" {
+  description = "One or more DNS names (or IP addresses) and TLS port pairs for VPC connectivity"
+  value       = module.msk_cluster.bootstrap_brokers_vpc_connectivity_tls
+}
+
 output "cluster_name" {
   description = "Name of the MSK cluster"
   value       = module.msk_cluster.cluster_name
