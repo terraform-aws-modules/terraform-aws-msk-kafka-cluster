5aa5a1ec528d4dddd476b84794bda20bddcdf024
magreenbaum <melissaafgreenbaum@gmail.com>
2025-12-12 13:36:23 -0700

    revert

--- examples/serverless/main.tf
+++ examples/serverless/main.tf
@@ -6,7 +6,7 @@ data "aws_availability_zones" "available" {}
 
 locals {
   name   = "ex-${basename(path.cwd)}"
-  region = "us-west-2"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
