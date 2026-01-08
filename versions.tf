e27658445859d5d1fc12e3f4b9c3e56a0d84c92f
Bryant Biggs <bryantbiggs@gmail.com>
2026-01-08 13:20:19 -0600

    feat: Add provider meta user-agent (#61)

--- versions.tf
+++ versions.tf
@@ -4,11 +4,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.22.1"
+      version = ">= 6.28"
     }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.6"
     }
+  }
+
+  provider_meta "aws" {
+    user_agent = [
+      "github.com/terraform-aws-modules/terraform-aws-msk-kafka-cluster"
+    ]
   }
 }
